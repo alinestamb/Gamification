20f1ab01443fcb60d8c7202b50c03077f20f5fca
Rida <ridakharrl@gmail.com>
2023-09-25 13:21:03 +1000

    changes to frontend

--- gamification/src/App.js
+++ gamification/src/App.js
@@ -1,32 +1,27 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/navbar.js';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-import startlearning from './mainpages/startlearning';
-import howtoplay from './mainpages/howtoplay';
-import aboutus from './mainpages/aboutus';
+// import Navbar from './components/navbar.js';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Startlearning from './mainpages/startlearning';
+import Howtoplay from './mainpages/howtoplay';
+import Aboutus from './mainpages/aboutus';
+import Profile from './mainpages/profile';
 
-
+ 
 function App() {
   return (
     <Router>
-      <div>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/start-learning" component={startlearning} />
-          <Route path="/how-to-play" component={howtoplay} />
-          <Route path="/about-us" component={aboutus} />
-        </Switch>
-      </div>
+        {/* <Navbar /> */}
+        <Routes>
+          <Route path="/aboutus" element={<Aboutus />} />
+          <Route path="/howtoplay" element={<Howtoplay />} />
+          <Route path="/startlearning" element={<Startlearning />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
     </Router>
   );
 }
 
-const HomePage = () => {
-  return <h1>Welcome to the Home Page</h1>;
-};
-
 export default App;
 
+ 
---
+++ gamification/src/fonts/robotomono.js
@@ -0,0 +1,9 @@
+import {createTheme } from '@mui/material';
+const Robotomono = createTheme({
+    typography:{
+        fontFamily: ['Roboto Mono','monospace'
+        ].join(","),
+    },
+});
+export default Robotomono; 
+
