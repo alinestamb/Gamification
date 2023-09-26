16e3460914248485b8e5e162674802726ba5467d
LaraStamboulian <79739278+LaraStamboulian@users.noreply.github.com>
2023-09-26 11:36:06 +1000

    Revert "Feature"

--- gamification/src/fonts/robotomono.js
+++
@@ -1,9 +0,0 @@
-import {createTheme } from '@mui/material';
-const Robotomono = createTheme({
-    typography:{
-        fontFamily: ['Roboto Mono','monospace'
-        ].join(","),
-    },
-});
-export default Robotomono; 
-
