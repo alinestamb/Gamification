84c3da42f264c1db3bb6526a633b2bee11d263fd
LaraStamboulian <lara.s.stamboulian@student.uts.edu.au>
2023-10-28 17:49:47 +1100

    disabling chapters + updating profile

--- gamification/src/mainpages/profile.js
+++ gamification/src/mainpages/profile.js
@@ -1,5 +1,5 @@
 // this page is to create the profile/avatar 
-import React from 'react'
+import React , {useState, useEffect} from 'react'
 import Navbar from '../components/navbar'
 import { Box, Grid, Typography, List, ListItem, Item, Button} from '@mui/material';
 // import Robotomono from '../fonts/robotomono';
@@ -8,9 +8,90 @@ import avatar from '../assets/avatar.png';
 import TextField from '@mui/material/TextField';
 import courseImg from '../assets/ai.png'; // Import the image
 
+const userId = "1";
+let setName = "";
+let setLink = "";
+let setBio = "";
+
+
+
 
 
 const Profile = () => {
+    let [completed, setCompleted] = useState(null);
+    let [dailyStreak, setDailyStreak] = useState(null);
+    let [points, setPoints] = useState(null);
+    let [UserName, setName] = useState(null);
+    let [badges, setBadges] = useState([]);
+    let [bio, setBio] = useState(null);
+    let [link, setLink] = useState(null);
+    let [courseName, setCourseName] = useState(null);
+    let BadgeName = "";
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setName(UserName);
+        setBio (bio);
+        setLink (link);
+        // data to Update to be the values in the text fields on handleChangeValue probable useState
+        const dataToUpdate={
+            name: UserName,
+            links: bio,
+            bio: link,
+        }
+        const jsonString = JSON.stringify(dataToUpdate);
+    
+        const url = "http://localhost:3002/" +userId;
+    
+        const options = {
+            method: 'PATCH',
+            headers : {
+                'Content-Type': 'application/json'
+            },
+            body : jsonString
+        }
+    
+        fetch (url, options)
+        .then(response => {
+            if (!response.ok)
+            {
+                throw new Error(`HIIP error ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(updatedData => {
+            console.log('Data updated: ', updatedData);
+        })
+        .catch (error => {
+            console.log('Error updating data:', error);
+        });
+        
+    
+    };
+    
+    useEffect(() => {
+      fetch("http://localhost:3002/" +userId, {
+        method: "GET",
+        
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          setCompleted(data.completed);
+          setDailyStreak(data.dailyStreak);
+          setPoints(data.points);
+          setName(data.name);
+          setBadges(data.badges);
+          setBio(data.bio);
+          setLink(data.links);
+          setCourseName(data.enrolled);
+          
+          console.log(data);
+        })
+        .catch((error) => console.log(error));
+    }, []);
+
+
+
     return(
         <Box sx={{ flexGrow: 1, backgroundColor:"#ffe", }}>
         <Navbar />
@@ -24,7 +105,7 @@ const Profile = () => {
                 <Grid item xs={4} md={4} direction={"column"} alignItems={"center"} > 
                 <Grid style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                     <img src={avatar} alt="avatar" style={{ height: '270px', width: 'auto' }} />
-                 <Typography sx={{ fontFamily: "Gamer" }}>GUEST USER</Typography>
+                 <Typography sx={{ fontFamily: "Gamer" }}>{UserName}</Typography>
                 </Grid>
                 </Grid>
                 <Grid item xs={9} md={3} sx={{ fontFamily: "RobotoMonoV", fontSize: "10px" }}>
@@ -35,8 +116,8 @@ const Profile = () => {
                     <Grid item xs={12} md={9}>
                         <TextField
                         id="demo-helper-text-aligned"
-                        label="NAME"
-                        defaultValue="Guest User"
+                        value={UserName}
+                        onChange = {(e) => setName(e.target.value)}
                         style={{ width: "500px", backgroundColor:"#E5E5E5" }}
                         />
                     </Grid>
@@ -46,8 +127,8 @@ const Profile = () => {
                 <Grid item xs={12} md={9}>
                     <TextField
                     id="demo-helper-text-aligned"
-                    label="BIO"
-                    defaultValue="Here to learn! Love stargazing and thinking about how AI can change the world XD"
+                    value={bio}
+                    onChange = {(e) => setBio(e.target.value)}
                     multiline
                     rows={3}
                     style={{ width: "500px", backgroundColor:"#E5E5E5" }}
@@ -59,14 +140,18 @@ const Profile = () => {
                 <Grid item xs={12} md={9}>
                     <TextField
                     id="demo-helper-text-aligned"
-                    label="LINKS"
-                    defaultValue="https://github.com/alinestamb/Gamification"
+                    value={link}
+                    onChange = {(e) => setLink(e.target.value)}
                     style={{ width: "500px", backgroundColor:"#E5E5E5" }}
                     />
                 </Grid>
+                
                 </Grid>
+                
                 </Grid>
+                <Button onClick = {handleSubmit} style={{marginLeft:"40%",  border: '2px solid #c1c1c1', backgroundColor: "#e5e0e0", padding:'5px'}}><Typography fontFamily="Gamer" color={"#7B7A7A"}>UPDATE DETAILS</Typography></Button>
             </Grid>
+           
             <Grid container spacing={2} style={{margin:"50px", marginLeft:"100px"}}> 
 {/* This is the box for enrolled courses */}
             <Grid container spacing={2} style={{ marginLeft: "100px" }}>
@@ -79,7 +164,7 @@ const Profile = () => {
                     <Grid sx={{ position: "relative", textAlign: "center", color: "white" }}>
                     <Button sx={{ color: "white" }}>
                         <img src={courseImg} style={{ width: '300px', height: "90px", borderRadius: "13px", border: "4px solid white", opacity: 0.73 }} />
-                        <Typography sx={{ fontFamily: "Gamer", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}> Introduction to Artifical Intelligence</Typography>
+                        <Typography sx={{ fontFamily: "Gamer", position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }}> {courseName}</Typography>
                     </Button>
                     </Grid>
                 </Grid>
@@ -90,20 +175,19 @@ const Profile = () => {
                 <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} >
                 <Grid item xs={4} p={1} sx={{backgroundColor: "#e5e0e0", border: "4px solid #EB1717", marginBottom:"10px", margin:"40px", width:"200px", height:"90px", fontSize:"10px"}}>
                     <Typography fontFamily={"Gamer"} textAlign={"center"}>Challenges Completed</Typography>
-                    <Typography sx = {{fontFamily:"Gamer", fontSize:"30px", textAlign:"center"}}>1</Typography>
+                    <Typography sx = {{fontFamily:"Gamer", fontSize:"30px", textAlign:"center"}}>{completed}</Typography>
                 </Grid>
                 <Grid item xs={4} p={1} sx={{backgroundColor: "#e5e0e0",border: "4px solid #1957E0", marginBottom:"10px", margin:"40px", width:"200px", height:"90px", fontSize:"10px"}}>
                 <Typography fontFamily={"Gamer"} textAlign={"center"}>Total XP Earned</Typography>
-                <Typography sx = {{fontFamily:"Gamer", fontSize:"30px", textAlign:"center"}}>1</Typography>
+                <Typography sx = {{fontFamily:"Gamer", fontSize:"30px", textAlign:"center"}}>{points}</Typography>
                 </Grid>
                 <Grid item xs={4} p={1} sx={{backgroundColor: "#e5e0e0",border: "4px solid #9F10E7", marginBottom:"10px",margin:"40px", width:"200px", height:"90px", fontSize:"10px"}}> 
                 <Typography fontFamily={"Gamer"} textAlign={"center"}>Badges Collected </Typography>
-                <Typography sx = {{fontFamily:"Gamer", fontSize:"30px", textAlign:"center"}}>1</Typography>
-
+                <Typography sx = {{fontFamily:"Gamer", fontSize:"30px", textAlign:"center"}}>{badges.length}</Typography>                  
                 </Grid>
                 <Grid item xs={4} p={1} sx={{backgroundColor: "#e5e0e0",border: "4px solid #4CD964", marginBottom:"10px",margin:"40px", width:"200px", height:"90px", fontSize:"10px"}}>
                 <Typography fontFamily={"Gamer"} textAlign={"center"}>Day Streak</Typography>
-                <Typography sx = {{fontFamily:"Gamer", fontSize:"30px", textAlign:"center"}}>1</Typography>
+                <Typography sx = {{fontFamily:"Gamer", fontSize:"30px", textAlign:"center"}}>{dailyStreak}</Typography>
 
                 </Grid>
             </Grid>
